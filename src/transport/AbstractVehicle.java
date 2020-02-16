c3d9fffb84611c71a8802f96761aece956a5df87
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-16 15:48:34 +0400

    created Abstract Class

---
+++ src/transport/AbstractVehicle.java
@@ -0,0 +1,29 @@
+package transport;
+
+public abstract class AbstractVehicle {
+    protected int fuel;
+    
+    public AbstractVehicle(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public AbstractVehicle() {
+        fuel = 1;
+    }
+
+    public abstract String getPath();
+    public abstract String getName();
+
+    public void move() {
+        fuel--;
+    }
+
+    public int getFuelLevel() {
+        return fuel;
+    }
+
+    public void addFuel(int i) {
+        fuel = fuel + i;
+    }
+
+}
