05069e47b7180c8cc4a2abb863c59994ec4621b0
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-16 15:35:56 +0400

    Printed out camel data

--- src/transport/Main.java
+++ src/transport/Main.java
@@ -5,6 +5,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println("*** INTERFACES ***");
+        System.out.println("*** Camels ***");
+
+        Camel granny = new Camel("granny");
+        Camel ferb = new Camel("ferb");
+        Camel junior = new Camel("Junior");
+
+        granny.eat(5);
+        ferb.eat(15);
+        for (int i = 0; i < 3; i++) {
+            granny.move();
+        }
+
+        System.out.println("Granny Fuel Level: " + granny.getFuelLVL());
     }
 }
